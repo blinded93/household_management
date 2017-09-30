39ae3671841dd6ecfaa2912327a3611d1590e7e8
blinded93 <blinded93@gmail.com>
2017-09-30 12:25:32 -0700

    created action structure with before action member set and loggin redirect

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,39 @@
 class MembersController < ApplicationController
+  before_action :set_member, only: [:show, :edit, :update, :destroy]
+  before action :redirect_unless_logged_in, only: [:show, :edit, :update, :destroy]
+  def index
+    @members = current_household.members
+  end
+
+  def show
+  end
+
+  def new
+    @member = Member.new
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+    def set_member
+      @member = Member.find_by(id: params[:id])
+    end
+
+    def member_params
+      params.require(:member).permit(:email, :password)
+    end
 end
