5edc807bc51ffd7e6ba664fb1562dd7b683feba5
blinded93 <blinded93@gmail.com>
2017-09-30 14:45:15 -0700

    added a household settings route

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
-  get 'welcome/home'
-
   root 'welcome#home'
   resources :members, only: [:create]
   get '/join' => 'members#new'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
+
   resources :households, only: [:show, :new, :create, :edit, :update, :destroy] do
+    get 'settings' => 'households#settings'
     resources :members
     resources :rooms
     resources :bills
   end
+
   resources :lists, only: [:create, :update, :destroy]
   resources :items, only: [:create, :update, :destroy]
 end
