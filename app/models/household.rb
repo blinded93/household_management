334b1f8184e02267310943645e1e9f7dafb8b2f0
blinded93 <blinded93@gmail.com>
2017-10-01 14:14:30 -0700

    add validations and custom member writer method

--- app/models/household.rb
+++ app/models/household.rb
@@ -4,5 +4,12 @@ class Household < ActiveRecord::Base
   has_many :rooms
 
   validates :name, presence: true
-  validates :address, presence: true, uniqueness: true
+  validates :address1, presence: true, uniqueness: true
+  validates :city, :state, :zip_code, presence: true
+
+  def members_attributes=(members_attributes)
+    members_attributes.values.each do |member_attributes|
+      Member.find_or_create_from_household_by(self, member_attributes)
+    end
+  end
 end
