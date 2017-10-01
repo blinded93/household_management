6937ae4997556723a6d4f57e7a5e202586acf881
blinded93 <blinded93@gmail.com>
2017-10-01 14:15:59 -0700

    add association, scope method and custom find or create method for Household members_attributes= method

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,4 +5,15 @@ class Member < ActiveRecord::Base
   belongs_to :household
   has_many :chores
   has_one :room
+  has_one :list, as: :list_owner
+
+  scope :part_of, -> (household) { where(household_id: household.id)}
+
+  def self.find_or_create_from_household_by(house, attrs)
+    if !member = self.find_by(email: attrs[:email])
+      house.members.build(attrs)
+    else
+      member
+    end
+  end
 end
