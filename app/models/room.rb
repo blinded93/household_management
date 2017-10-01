84d74eb2d2b5e3823481b52590ddc6e987802811
blinded93 <blinded93@gmail.com>
2017-10-01 14:17:17 -0700

    add associations

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,7 +2,8 @@ class Room < ActiveRecord::Base
   # include Meta
 
   belongs_to :household
-  has_one :lists, as: :list_owner
+  belongs_to :member
+  has_one :list, as: :list_owner
   has_many :room_items
   has_many :items, through: :room_items
 end
