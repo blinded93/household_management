106a786c238c5d4064987fc15c34dde86195530a
blinded93 <blinded93@gmail.com>
2017-10-05 16:58:06 -0700

    chore#complete action and set_chores helper and before_action added

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -1,5 +1,30 @@
 class ChoresController < ApplicationController
+  before_action :set_chores, only:[:index]
+
   def index
+  end
+
+  def complete
+    set_chores
+    # byebug
+    @chores.each do |chore|
+      if params[:chore_ids].try(:include?, chore.id.to_s)
+        chore.completed = true
+        chore.save
+      else
+        chore.completed = false
+        chore.save
+      end
+    end
+    redirect_to :back
+  end
+
+  def update
+
+  end
+
+  private
+  def set_chores
     @chores = Chore.send(params[:scope]).for(current_member)
   end
 end
