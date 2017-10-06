3f41e61898e82c78421547c05287cf790fef2cdd
blinded93 <blinded93@gmail.com>
2017-10-05 17:06:30 -0700

    add size method, counting items in list

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,8 @@ class List < ActiveRecord::Base
   belongs_to :list_owner, polymorphic:true
   has_many :list_items
   has_many :items, through: :list_items
+
+  def size
+    items.count
+  end
 end
