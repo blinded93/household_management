719cd917140bd8e35fd98f7b9807be5e4baeff76
blinded93 <blinded93@gmail.com>
2017-10-05 17:04:37 -0700

    add scopes 'completed' and 'not due'

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,10 +1,11 @@
 class Chore < ActiveRecord::Base
   belongs_to :member
 
-  scope :over_due, -> { where("due_date < ?", Date.today)}
+  scope :over_due, -> { where("due_date < ?", Date.today) }
   scope :due_today, -> { where(due_date: Date.today) }
   scope :due_tomorrow, -> { where(due_date: Date.tomorrow) }
-  scope :not_due, -> { where("due_date > ?", Date.tomorrow)}
+  scope :not_due, -> { where("due_date > ?", Date.tomorrow) }
+  scope :completed, -> { where(completed: true) }
   scope :for, -> (member) { where(member: member) }
 
   def self.scopes
@@ -12,12 +13,12 @@ class Chore < ActiveRecord::Base
       over_due: "Over Due",
       due_today: "Due Today",
       due_tomorrow: "Due Tomorrow",
-      not_due: "Not Due"
+      not_due: "Not Due",
+      completed: "Completed"
     }
   end
 
   def formatted_due_date
     due_date.strftime("%D")
   end
-
 end
