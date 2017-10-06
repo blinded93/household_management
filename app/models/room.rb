f51bf61456d5a121d188fcba50ff0c8702309e62
blinded93 <blinded93@gmail.com>
2017-10-05 17:03:46 -0700

    auto assign list to room

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,9 +1,15 @@
 class Room < ActiveRecord::Base
-  # include Meta
+  include Shared
 
   belongs_to :household
   belongs_to :member
   has_one :list, as: :list_owner
   has_many :room_items
   has_many :items, through: :room_items
+
+  after_initialize :assign_list
+
+  def assign_list
+    self.list || create_list(name: self.name)
+  end
 end
