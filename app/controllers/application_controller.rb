88c9f06848b4d0a70fe553854f4ebc9125c43d29
blinded93 <blinded93@gmail.com>
2017-10-08 22:11:13 -0700

    add helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_member, :current_household, :logged_in?
+  helper_method :current_member, :current_household, :logged_in?, :head_of_household?, :menu_partial
 
   def current_member
     @current_member ||= Member.find_by(id: session[:member_id])
@@ -18,5 +18,13 @@ class ApplicationController < ActionController::Base
     unless logged_in?
       redirect_to login_path, notice:"You must be logged in."
     end
+  end
+
+  def head_of_household?
+    current_member.head_of_household
+  end
+
+  def menu_partial
+    @partial ||= session[:menu_partial]
   end
 end
