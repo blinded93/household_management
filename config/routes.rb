80f6365e6338fce15ae0a4de3c2842a20fb5b2a3
blinded93 <blinded93@gmail.com>
2017-10-08 22:14:48 -0700

    change routes to reflect separation of household control from member access.

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,21 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
   post '/chores/complete' => 'chores#complete'
-  get '/chores/:scope' => 'chores#index'
+
   resources :households, only: [:show, :create, :edit, :update, :destroy] do
     get 'control' => 'households#control'
-    resources :members
+    resources :members, except: [:show]
     resources :rooms
     resources :bills
+    get '/chores/:scope' => 'chores#index'
   end
 
+  resources :members, only: [:show] do
+    get '/chores/:scope' => 'chores#index'
+    get '/messages/inbox' => 'messages#inbox'
+    get '/messages/sent' => 'messages#sent'
+    resources :messages, only: [:new, :create, :destroy]
+  end
   resources :chores, only: [:new, :create, :edit, :update, :destroy]
   resources :lists, only: [:edit, :update, :destroy]
   resources :items, only: [:create, :edit, :update, :destroy]
