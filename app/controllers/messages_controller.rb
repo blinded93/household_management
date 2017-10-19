47e89337c950fe4353e44c158d5fa3e8db39e251
blinded93 <blinded93@gmail.com>
2017-10-19 16:20:57 -0700

    fill out messages controller with callback, #new/#create and private helper methods.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,41 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:show]
   def inbox
     @messages = current_member.recieved_messages
+    render :index
   end
 
   def sent
     @messages = current_member.sent_messages
+    render :index
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.valid?
+      @message.save
+    else
+      render :new
+    end
   end
 
   def show
+    if !@message.read_at
+      @message.read_at = Time.now
+      @message.save
+    end
   end
+
+  private
+    def set_message
+      @message = Message.find_by(id: params[:id])
+    end
+
+    def message_params
+      params.require(:message).permit(:subject, :body, :sender_id, :recipient_id)
+    end
 end
