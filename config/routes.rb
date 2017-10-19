5c7e30bf91ab73f0caaff6c5674c83853fac9225
blinded93 <blinded93@gmail.com>
2017-10-19 16:17:11 -0700

    add prefixes to chores#index, messages#inbox/sent, adjusted chore complete/clear routes to coincide with new layout.

--- config/routes.rb
+++ config/routes.rb
@@ -4,23 +4,24 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
-  post '/chores/complete' => 'chores#complete'
+  patch '/chores/:id/complete' => 'chores#complete', as: :complete_chores
+  delete '/chores/clear' => 'chores#destroy', as: :clear_chores
 
   resources :households, only: [:show, :create, :edit, :update, :destroy] do
     get 'control' => 'households#control'
     resources :members, except: [:show]
     resources :rooms
     resources :bills
+    resources :chores, only: [:new, :create, :edit, :update, :destroy]
     get '/chores/:scope' => 'chores#index'
   end
 
   resources :members, only: [:show] do
-    get '/chores/:scope' => 'chores#index'
-    get '/messages/inbox' => 'messages#inbox'
-    get '/messages/sent' => 'messages#sent'
+    get '/chores/:scope' => 'chores#index', as: :chores
+    get '/messages/inbox' => 'messages#inbox', as: :inbox
+    get '/messages/sent' => 'messages#sent', as: :sent_messages
     resources :messages, only: [:new, :create, :destroy]
   end
-  resources :chores, only: [:new, :create, :edit, :update, :destroy]
   resources :lists, only: [:edit, :update, :destroy]
   resources :items, only: [:create, :edit, :update, :destroy]
 end
