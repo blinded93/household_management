b89230f4275f8c41d194991bf5ebc5d7e6402c9e
blinded93 <blinded93@gmail.com>
2017-10-19 16:03:25 -0700

    add validation for future date and change scopes

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,10 +1,13 @@
 class Chore < ActiveRecord::Base
   belongs_to :member
 
+  validate :after_today, on: :create
+
+  default_scope { order(due_date: :desc) }
   scope :over_due, -> { where("due_date < ?", Date.today) }
   scope :due_today, -> { where(due_date: Date.today) }
-  scope :due_tomorrow, -> { where(due_date: Date.tomorrow) }
-  scope :not_due, -> { where("due_date > ?", Date.tomorrow) }
+  scope :due_tomorrow, -> { where(due_date: Date.today + 1) }
+  scope :future, -> { where("due_date > ?", Date.today + 1) }
   scope :completed, -> { where(completed: true) }
   scope :for, -> (household) {
     joins(:member).
@@ -16,12 +19,22 @@ class Chore < ActiveRecord::Base
       over_due: "Over Due",
       due_today: "Due Today",
       due_tomorrow: "Due Tomorrow",
-      not_due: "Not Due",
+      future: "Future",
       completed: "Completed"
     }
   end
 
+  # def self.chores_from_ids(member)
+  #   params.collect {|id| member.chores.find_by(id: id)}
+  # end
+
   def formatted_due_date
     due_date.strftime("%D")
+  end
+
+  def after_today
+    if due_date.present? && due_date < Date.today
+      errors.add(:due_date, "can't be in the past.")
+    end
   end
 end
