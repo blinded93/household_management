c1b2222e113e97bc35d9d3a2923ff3e59784700a
blinded93 <blinded93@gmail.com>
2017-10-28 19:20:00 -0700

    add #list_item to find an items/lists list_item, create ListItem methods to complete reversed nested form between list_item and item

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,8 @@ class Item < ActiveRecord::Base
   has_many :lists, through: :list_items
   has_many :room_items
   has_many :rooms, through: :room_items
+
+  def list_item(list)
+    self.list_items.find_by(list_id:list.id)
+  end
 end
--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -1,4 +1,22 @@
 class ListItem < ActiveRecord::Base
   belongs_to :list
   belongs_to :item
+
+  def self.find_or_create(attrs, item)
+    search_attrs = attrs.select{|key, value| key != 'quantity' && !value.is_a?(Hash)}
+    search_attrs[:item_id] = item.id
+    if list_item = ListItem.find_by(search_attrs)
+      list_item.quantity += attrs[:quantity].to_i
+    else
+      list_item = ListItem.new(attrs)
+    end
+    list_item.save
+    list_item
+  end
+
+  def item_attributes=(attrs)
+    item = Item.create(attrs)
+    self.item = item
+    self.save
+  end
 end
