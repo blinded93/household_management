7c9e7350ecf9b3ab6b5060f0109ccc9f979ded48
blinded93 <blinded93@gmail.com>
2017-11-06 09:16:48 -0800

    add login capability for for invitees to assure new member creation is only done by invited users.

--- app/controllers/invitees_controller.rb
+++ app/controllers/invitees_controller.rb
@@ -1,7 +1,26 @@
 class InviteesController < ApplicationController
-  before_action :set_invitee, only: [:edit, :update, :destroy]
+  before_action :set_invitee, only: [:show, :edit, :update, :destroy]
   before_action :redirect_unless_logged_in
   before_action :correct_parent
+
+  def login
+    household = Household.find_by(id:params[:household_id])
+    binding.pry
+    render layout:false
+  end
+
+  def verify
+    @invitee = Invitee.find_by(email: params[:email])
+    if @invitee && @invitee.authenticate(params[:password])
+      redirect_to [@invitee.household, :member, :new]
+    else
+      render :login
+    end
+  end
+
+  def show
+    @member = Member.new(first_name:params[:name], email:params[:email])
+  end
 
   def new
     @invitee = Invitee.new
@@ -10,6 +29,7 @@ class InviteesController < ApplicationController
   def create
     @invitee = current_household.invitees.build(invitee_params)
     @invitee.set_password
+    binding.pry
     if @invitee.valid?
       @invitee.save
       MemberMailer.invite(current_member, @invitee).deliver_now
