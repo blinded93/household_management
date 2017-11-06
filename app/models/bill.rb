8b503675623d8b458cc566779a5ac6abda0d7ffd
blinded93 <blinded93@gmail.com>
2017-11-06 09:19:40 -0800

    add scopes to bill model.

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,5 +1,22 @@
 class Bill < ActiveRecord::Base
-  # include Meta
-
   belongs_to :household
+
+  scope :over_due, -> { where("due_date < ?", Date.today) }
+  scope :within_week, -> { where(due_date: Date.today..Date.today + 1.week) }
+  scope :this_month, -> {
+    where(due_date: Date.today.beginning_of_month..Date.today.end_of_month)
+    }
+  scope :next_month, -> {
+    where(due_date: Date.today.next_month.beginning_of_month..Date.today.next_month.end_of_month)
+    }
+  scope :due_on, -> (date) { where(due_date: date) }
+
+  def self.scopes
+    {
+      over_due: "Over Due",
+      within_week: "Within 7 Days",
+      this_month: "This Month",
+      next_month: "Next Month"
+    }
+  end
 end
