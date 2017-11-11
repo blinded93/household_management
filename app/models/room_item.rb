bfc1b6fa5425177def4f3c4b1d702329c7fe0831
blinded93 <blinded93@gmail.com>
2017-11-11 11:58:33 -0800

    complete methods #find_or_create and #item_attributes= for room_items#create/update

--- app/models/room_item.rb
+++ app/models/room_item.rb
@@ -1,4 +1,25 @@
 class RoomItem < ActiveRecord::Base
   belongs_to :room
   belongs_to :item
+
+  def self.find_or_create(attrs, item)
+    search_attrs = attrs.select{|key, value| key != 'stock' && !value.is_a?(Hash)}
+    search_attrs[:item_id] = item.id
+    if room_item = RoomItem.find_by(search_attrs)
+      room_item.quantity += attrs[:stock].to_i
+    else
+      room_item = RoomItem.new(attrs)
+    end
+    room_item.save
+    room_item
+  end
+
+  def item_attributes(attrs)
+    if item = Item.find_by(attrs)
+      self.item = item
+    else
+      self.create_item(attrs)
+    end
+    self.save
+  end
 end
