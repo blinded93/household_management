9fe877e3d8e72fcd941b951e66c6f2ad31e12fd9
blinded93 <blinded93@gmail.com>
2017-11-11 11:28:59 -0800

    moved calendar helper from bills to calendar helper file.

--- app/helpers/bills_helper.rb
+++ app/helpers/bills_helper.rb
@@ -1,8 +1,4 @@
 module BillsHelper
-  def calendar(date = Date.today, &block)
-    Calendar.new(self, date, block).table
-  end
-
   def bill_sum(date)
     current_household.bills.due_on(date).sum(:amount)
   end
