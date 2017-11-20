e543c969f0bf1d600b5dd3dc7f54daf7f76ae611
blinded93 <blinded93@gmail.com>
2017-11-19 21:23:52 -0800

    add helpers to dynamically set item name or link depending on the view, refactor table columns

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,21 @@
 module ItemsHelper
+  def render_edit(item, addee)
+    render "#{addee.class_name}_items/edit", item:item, "#{addee.class_name}_item".to_sym => item.addee_item(addee)
+  end
+
+  def item_edit_link(item, addee)
+    link_to item.name,
+        [item.addee_item(addee)],
+        data:{toggle:'modal',
+              target:"#edit_item_#{item.id}"},
+        class:'btn btn-sm text-info'
+  end
+
+  def item_name(item, addee)
+    if addee.is_a?(List)
+      admin? ? item.name : item_edit_link(item, addee)
+    elsif addee.is_a?(Room)
+      admin? ? item_edit_link(item, addee) : item.name
+    end
+  end
 end
