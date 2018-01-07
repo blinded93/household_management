d75df55e281f922dbfe989939c8c66342995cb7e
blinded93 <blinded93@gmail.com>
2018-01-07 11:27:57 -0800

    change create/update actions to utilize js/ajax.  add :password to #household_params.

--- app/controllers/households_controller.rb
+++ app/controllers/households_controller.rb
@@ -1,6 +1,6 @@
 class HouseholdsController < ApplicationController
   before_action :redirect_unless_logged_in, except: [:new, :create]
-  before_action :set_household, only: [:control, :settings, :update, :destroy]
+  before_action :set_household, only: [:control, :update, :destroy]
   before_action :correct_parent, only: [:control]
 
   def control
@@ -20,21 +20,34 @@ class HouseholdsController < ApplicationController
 
   def create
     @household = Household.new(household_params)
-    if @household.valid?
-      @household.save
-      member = @household.members.first
-      session[:member_id] = member.id
-      redirect_to household_control_path(@household)
-    else
-      render 'welcome/home'
+    member = @household.members[0]
+    respond_to do |format|
+      if @household.save
+        session[:member_id] = @household.members[0].id
+        format.js
+      else
+        format.js { render 'errors',
+                    locals:{ household:@household,
+                             member:member
+                            }
+                   }
+      end
     end
   end
 
   def update
-    if @household.update(household_params)
-      redirect_to [current_household, :control, tab:'household']
-    else
-
+    respond_to do |format|
+      if @household.update(household_params)
+        format.js { render 'update',
+                    locals:{ obj:@household
+                            }
+                   }
+      else
+        format.js { render 'shared/errors',
+                    locals: { obj:@household
+                            }
+                   }
+      end
     end
   end
 
@@ -48,6 +61,6 @@ class HouseholdsController < ApplicationController
     end
 
     def household_params
-      params.require(:household).permit(:name, :address1, :address2, :city, :state, :zip_code, members_attributes: [:first_name, :last_name, :family_title, :monthly_income, :email, :password, :head_of_household])
+      params.require(:household).permit(:name, :address1, :address2, :city, :state, :zip_code, :password, members_attributes: [:first_name, :last_name, :family_title, :monthly_income, :email, :password, :head_of_household])
     end
 end
