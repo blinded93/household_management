f95f640be1f10733b96be8b58df687669b801dd3
blinded93 <blinded93@gmail.com>
2018-01-07 11:45:06 -0800

    change create/update and add inventory_request action to utilize js/ajax.

--- app/controllers/room_items_controller.rb
+++ app/controllers/room_items_controller.rb
@@ -2,22 +2,53 @@ class RoomItemsController < ApplicationController
   before_action :set_room_item, only: [:inventory_request, :update, :destroy]
 
   def create
-    @room_item = RoomItem.find_or_create(room_item_params)
-    redirect_to [current_household, :control, tab:'inventory']
-  end
-
-  def inventory_request
-    @room_item.request = true
-    @room_item.save
-    redirect_to [current_member, tab:'inventory']
+    @room_item = RoomItem.new(room_item_params)
+    respond_to do |format|
+      if @room_item.save
+        format.js { render 'shared/create',
+                    locals:{obj:@room_item}
+                  }
+      else
+        format.js { render 'shared/errors',
+                    locals:{obj:@room_item}
+                  }
+      end
+    end
   end
 
   def update
     if room_item_params[:stock].to_i > @room_item.stock
       @room_item.request = false
     end
-    @room_item.update(room_item_params)
-    redirect_to [current_household, :control]
+    respond_to do |format|
+      if @room_item.update(room_item_params)
+        format.js { render 'shared/update',
+                    locals:{obj:@room_item}
+                  }
+      else
+        format.js { render 'shared/errors',
+                    locals:{obj:@room_item}
+                  }
+      end
+    end
+  end
+
+  def inventory_request
+    locals = {room_item:@room_item,
+              a:params[:a]
+              }
+    if locals[:a] == "request"
+      @room_item.request = true
+      @room_item.save
+    else
+      @room_item.request = false
+      @room_item.save
+    end
+    respond_to do |format|
+        format.js { render 'request',
+                    locals:locals
+                  }
+    end
   end
 
   def destroy
@@ -31,6 +62,6 @@ class RoomItemsController < ApplicationController
     end
 
     def room_item_params
-      params.require(:room_item).permit(:stock, :room_id, :threshold, item_attributes:[:name])
+      params.require(:room_item).permit(:a, :stock, :room_id, :threshold, item_attributes:[:name])
     end
 end
