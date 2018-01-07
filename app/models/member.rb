612bda52cebb76fd49876313f12fd3ccfa928471
blinded93 <blinded93@gmail.com>
2018-01-07 11:12:30 -0800

    change #after_initialize callback to #after_create and added room creation and assignment. slightly change structure, but not function of a couple other methods.

--- app/models/member.rb
+++ app/models/member.rb
@@ -13,10 +13,15 @@ class Member < ActiveRecord::Base
   has_many :recieved_messages, class_name: 'Message', primary_key: 'id', foreign_key: 'recipient_id', dependent: :destroy
   has_many :sent_messages, class_name: 'Message', primary_key: 'id', foreign_key: 'sender_id', dependent: :destroy
 
-  after_initialize :assign_list
+  after_create :assign_list_and_room
 
-  def assign_list
-    self.list || create_list(name: self.name)
+  def assign_list_and_room
+    create_list(name: self.name)
+    create_room(name: self.name, household_id:self.household.id)
+  end
+
+  def self.scopes
+    {member.name.downcase.to_sym => member.name}
   end
 
   def self.find_or_create_from_household_by(house, attrs)
@@ -35,16 +40,24 @@ class Member < ActiveRecord::Base
   end
 
   def plural
-    self.name + "'s"
+    "#{self.name}'s"
   end
 
   def nested_id_name(field)
-    "household_members_attributes_0_" + field.to_s
+    "household_members_attributes_0_#{field.to_s}"
   end
 
   def positive_income
     if monthly_income < 0
       errors.add(:monthly_income, "cannot be below zero.")
     end
+  end
+
+  def reload_acc
+    "accordion_member"
+  end
+
+  def objects_hash
+    {members:self.household.members}
   end
 end
