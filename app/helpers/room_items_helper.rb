3b3f95ca33f95978f9fd3bc2ced83cc209d9bda2
blinded93 <blinded93@gmail.com>
2018-01-29 09:30:16 -0800

    change request method from changing border to tr background color, undo button for requesting items and method to determine if action is a request.

--- app/helpers/room_items_helper.rb
+++ app/helpers/room_items_helper.rb
@@ -3,9 +3,21 @@ module RoomItemsHelper
     room_items.need_purchased.count + room_items.with_requests.count
   end
 
-  def request_border(item, addee)
-    if item.addee_item(addee).try(:request)
-      'border border-danger'
+  def requested?(room_item)
+    if room_item.requested?
+      "table-danger"
     end
+  end
+
+  def request_action
+    params[:a] == "request" ? true : false
+  end
+
+  def undo_btn
+    link_to( 'Undo',
+      [current_member, @room_item, :request, a:'undo'],
+      class:'btn btn-sm text-danger pt-0',
+      method:'post',
+      remote:true).gsub("\"", "'").html_safe
   end
 end
