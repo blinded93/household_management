70d49b525c37ce3b66d2161dad81e8277fc5bfc8
blinded93 <blinded93@gmail.com>
2018-01-29 08:54:40 -0800

    nested list/room_item routes, created new/edit routes to utilize ajax with modals.

--- config/routes.rb
+++ config/routes.rb
@@ -4,24 +4,28 @@ Rails.application.routes.draw do
   delete '/logout' => 'sessions#destroy'
   patch '/chores/:id/complete' => 'chores#complete', as: :complete_chore
   delete '/chores/clear' => 'chores#destroy', as: :clear_chores
-  get '/calendar/load' => 'calendar#load'
 
-  resources :households, only: [:create, :update, :destroy] do
+  resources :households, except: [:index] do
+    get '/calendar/day' => 'calendar#day'
+    get '/calendar/load' => 'calendar#load'
     get 'control' => 'households#control'
     resources :members, except: [:show]
-    resources :rooms, only: [:new, :create, :update, :destroy]
-    resources :bills, only: [:create, :update, :destroy]
-    resources :chores, only: [:create, :update, :destroy]
+    resources :rooms, except: [:index]
+    resources :bills, except: [:index]
+    get '/chores/reload' => 'chores#reload'
+    resources :chores, except: [:index]
+    resources :list_items, only: [:destroy]
+    get '/list_items/reload' => 'list_items#reload'
+    resources :room_items, except: [:index]
   end
 
   resources :members, only: [:show] do
+    get '/chores/reload' => 'chores#reload'
     get '/messages/inbox' => 'messages#inbox', as: :inbox
     get '/messages/check' => 'messages#check', as: :check_messages
     post '/messages/reply' => 'messages#reply'
-    resources :messages, only: [:create, :destroy]
+    resources :messages, except: [:index, :update]
+    resources :list_items, except: [:index]
+    post 'request/room_item/:id' => 'room_items#inventory_request', as: :room_item_request
   end
-
-  resources :list_items, only: [:create, :update, :destroy]
-  post 'request/room_item/:id' => 'room_items#inventory_request', as: :room_item_request
-  resources :room_items, only: [:create, :update, :destroy]
 end
