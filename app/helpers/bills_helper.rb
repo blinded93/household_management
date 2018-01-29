f525931970569afcbf9eca5cd7fab39aa6451ee0
blinded93 <blinded93@gmail.com>
2018-01-29 09:00:47 -0800

    method to add all bills under a specific scope.

--- app/helpers/bills_helper.rb
+++ app/helpers/bills_helper.rb
@@ -2,4 +2,8 @@ module BillsHelper
   def bill_sum(date)
     current_household.bills.due_on(date).sum(:amount)
   end
+
+  def scope_bill_sum(bills, scope)
+    number_to_currency(bills.send(scope).sum(:amount))
+  end
 end
