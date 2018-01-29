33caec5a83ca65614d36f9fce273670fe48bc407
blinded93 <blinded93@gmail.com>
2018-01-29 09:03:47 -0800

    remove unused methods.

--- app/helpers/households_helper.rb
+++ app/helpers/households_helper.rb
@@ -2,14 +2,4 @@ module HouseholdsHelper
   def show_tab(tab)
     'show' if tab == selected_tab
   end
-
-  def show_scope(scope)
-    'show' if scope == selected_scope
-  end
-
-  def house_redirect(tab, scope)
-    hash = {tab:tab}
-    hash[:scope] = scope if scope
-    household_control_path(current_household, hash)
-  end
 end
