d3136aa4af1dff79d15807da46ac0ab00a4e24e8
blinded93 <blinded93@gmail.com>
2018-01-29 18:59:44 -0800

    refactored for beautification. add new/edit actions for ajax modals. created ajax destroy action.

--- app/controllers/room_items_controller.rb
+++ app/controllers/room_items_controller.rb
@@ -1,59 +1,100 @@
 class RoomItemsController < ApplicationController
-  before_action :set_room_item, only: [:inventory_request, :update, :destroy]
+  before_action :set_room_item, only: [:inventory_request, :edit, :update, :destroy]
+
+  def new
+    @room_item = RoomItem.new
+    @item = @room_item.build_item
+    @items = Item.all.pluck(:name)
+    respond_to do |format|
+      format.js {
+        render 'shared/new_edit',
+        locals:{obj:@room_item}
+      }
+    end
+  end
 
   def create
     @room_item = RoomItem.new(room_item_params)
     respond_to do |format|
       if @room_item.save
-        format.js { render 'shared/create',
-                    locals:{obj:@room_item}
-                  }
+        format.js {
+          render 'create',
+          locals:{
+            room_item:@room_item,
+            room:@room_item.room
+          }
+        }
       else
-        format.js { render 'shared/errors',
-                    locals:{obj:@room_item}
-                  }
+        format.js {
+          render 'shared/errors',
+          locals:{obj:@room_item}
+        }
       end
     end
   end
 
+  def edit
+    @items = Item.all.pluck(:name)
+    respond_to do |format|
+      format.js {
+        render 'shared/new_edit',
+        locals:{obj:@room_item}
+      }
+    end
+  end
+
   def update
+    @old_scopes = @room_item.scopes.dup
     if room_item_params[:stock].to_i > @room_item.stock
       @room_item.request = false
     end
     respond_to do |format|
       if @room_item.update(room_item_params)
-        format.js { render 'shared/update',
-                    locals:{obj:@room_item}
-                  }
+        format.js {
+          render 'update',
+          locals:{
+            room_item:@room_item,
+            room:@room_item.room
+          }
+        }
       else
-        format.js { render 'shared/errors',
-                    locals:{obj:@room_item}
-                  }
+        format.js {
+          render 'shared/errors',
+          locals:{obj:@room_item}
+        }
       end
     end
   end
 
   def inventory_request
-    locals = {room_item:@room_item,
-              a:params[:a]
-              }
+    locals = {
+      room_item:@room_item,
+      a:params[:a]
+    }
     if locals[:a] == "request"
-      @room_item.request = true
-      @room_item.save
+      @room_item.toggle!(:request)
     else
       @room_item.request = false
       @room_item.save
     end
     respond_to do |format|
-        format.js { render 'request',
-                    locals:locals
-                  }
+        format.js {
+          render 'request',
+          locals:locals
+        }
     end
   end
 
   def destroy
-    @room_item.destroy
-    redirect_to :back
+    respond_to do |format|
+      locals = {objs:RoomItem.for(current_member)}
+      locals[:obj] = @room_item
+      @room_item.delete
+      format.js {
+        render 'shared/delete',
+        locals:locals
+      }
+    end
   end
 
   private
@@ -62,6 +103,6 @@ class RoomItemsController < ApplicationController
     end
 
     def room_item_params
-      params.require(:room_item).permit(:a, :stock, :room_id, :threshold, item_attributes:[:name])
+      params.require(:room_item).permit(:a, :stock, :room_id, :threshold, item_attributes:[:name, :id])
     end
 end
