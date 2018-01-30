3dc60bdee2ce6e15e24f446afe0e58208d9efbfc
blinded93 <blinded93@gmail.com>
2018-01-29 18:40:04 -0800

    changed ajax calls to use their own create/update files, for a more simplistic approach. beautified code.  add ajax call for destroy action.

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,40 +1,71 @@
 class BillsController < ApplicationController
   before_action :set_bill, only: [:update, :destroy]
   before_action :redirect_unless_logged_in
-  before_action :correct_parent
+
+  def new
+    @bill = Bill.new
+    respond_to do |format|
+      format.js {
+        render 'shared/new_edit',
+        locals:{obj:@bill}
+      }
+    end
+  end
 
   def create
     @bill = current_household.bills.build(bill_params)
     respond_to do |format|
       if @bill.save
-        format.js { render 'shared/create',
-                    locals:{obj:@bill}
-                  }
+        format.js {
+          render 'create',
+          locals:{bill:@bill}
+        }
       else
-        format.js { render "shared/errors",
-                    locals:{obj:@bill}
-                  }
+        format.js {
+          render "shared/errors",
+          locals:{obj:@bill}
+        }
       end
     end
   end
 
+  def edit
+    @bill = Bill.find_by(id: params[:id])
+    respond_to do |format|
+      format.js {
+        render 'shared/new_edit',
+        locals:{obj:@bill}
+      }
+    end
+  end
+
   def update
+    @old_scopes = @bill.scopes.dup
     respond_to do |format|
       if @bill.update(bill_params)
-        format.js { render 'shared/update',
-                    locals:{obj:@bill}
-                  }
+        format.js {
+          render 'update',
+          locals:{bill:@bill}
+        }
       else
-        format.js { render "shared/errors",
-                    locals:{obj:@bill}
-                  }
+        format.js {
+          render "shared/errors",
+          locals:{obj:@bill}
+        }
       end
     end
   end
 
   def destroy
-    @bill.destroy
-    redirect_to :back
+    respond_to do |format|
+      locals = {objs:current_household.bills}
+      locals[:obj] = @bill
+      @bill.destroy
+      format.js {
+        render 'shared/delete',
+        locals:locals
+      }
+    end
   end
 
   private
