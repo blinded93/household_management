533f717416c7d15fa651050c7d2e82d0d9a017df
blinded93 <blinded93@gmail.com>
2018-01-29 18:41:30 -0800

    create 'day' action for ajax call to open dialog modal, instead of preloading them all. make 'load' action more concise.

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,9 +1,17 @@
 class CalendarController < ApplicationController
+  def day
+    @date = params[:date] ? Date.parse(params[:date]) : Date.today
+    @events = current_household.bills.group_by(&:due_date).merge(Chore.for(current_household).group_by(&:due_date))
+    respond_to do |format|
+      format.js
+    end
+  end
+
   def load
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
     @events = current_household.bills.group_by(&:due_date).merge(Chore.for(current_household).group_by(&:due_date))
     respond_to do |format|
-      format.js { render 'calendar/load' }
+      format.js
     end
   end
 end
