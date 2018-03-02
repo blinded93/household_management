3c9bf5d47b792fa191ab97b61d0839168531ada5
blinded93 <blinded93@gmail.com>
2018-03-01 18:40:00 -0800

    change #scope_bill_sum to #remaining_expenses. add helper for button to mark a bill as being paid.

--- app/helpers/bills_helper.rb
+++ app/helpers/bills_helper.rb
@@ -3,7 +3,18 @@ module BillsHelper
     current_household.bills.due_on(date).sum(:amount)
   end
 
-  def scope_bill_sum(bills, scope)
-    number_to_currency(bills.send(scope).sum(:amount))
+  def mark_bill_as_paid_btn(bill)
+    if bill.is_a?(Bill) && bill.persisted?
+      (link_to (bill.paid ? "Mark as Unpaid" : "Mark as Paid"),
+        pay_household_bill_path(bill),
+        class:"btn btn-sm btn-outline-success text-dark ml-2",
+        method:'patch',
+        remote:true).html_safe
+    end
+  end
+
+  def remaining_expenses
+    bills = current_household.bills
+    number_to_currency(bills.this_month.sum(:amount) + bills.unpaid.sum(:amount))
   end
 end
