9b75f7afaf1779c3223303e47fe31963ddacb859
blinded93 <blinded93@gmail.com>
2018-03-01 18:42:12 -0800

    add #chores_hash method to clean up view and conditionally add chores variable if chores variable is set.

--- app/helpers/chores_helper.rb
+++ app/helpers/chores_helper.rb
@@ -14,4 +14,9 @@ module ChoresHelper
       str + " badge-success"
     end
   end
+
+  def chores_hash(scope, scope_title, chores)
+    hash = {scope:scope, scope_title:scope_title, chores:[]}
+    chores.empty? ? hash : hash.merge({chores:chores})
+  end
 end
