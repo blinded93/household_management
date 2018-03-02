0b9dd4dcff3976b7037414854408ad16d1824b01
blinded93 <blinded93@gmail.com>
2018-03-01 18:28:54 -0800

    remove repeating variable declarations in order to DRY out code.

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,17 +1,24 @@
 class CalendarController < ApplicationController
   def day
-    @date = params[:date] ? Date.parse(params[:date]) : Date.today
-    @events = current_household.bills.group_by(&:due_date).merge(Chore.for(current_household).group_by(&:due_date))
+    set_events_and_date
     respond_to do |format|
       format.js
     end
   end
 
   def load
-    @date = params[:date] ? Date.parse(params[:date]) : Date.today
-    @events = current_household.bills.group_by(&:due_date).merge(Chore.for(current_household).group_by(&:due_date))
+    set_events_and_date
     respond_to do |format|
       format.js
     end
+  end
+
+  private
+  def set_events_and_date
+    @date = params[:date] ? Date.parse(params[:date]) : Date.today
+    @bills = current_household.bills.group_by(&:due_date)
+    @chores = Chore.for(current_household)
+    @chores_by_date = @chores.group_by(&:due_date)
+    @events = @bills.merge(@chores_by_date)
   end
 end
