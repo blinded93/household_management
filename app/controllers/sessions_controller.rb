cfde6b3de64b2b0c0009ff4a91082231c1200486
blinded93 <blinded93@gmail.com>
2018-03-01 18:36:19 -0800

    add auth method to wrap omniauth hash. changed login #create method to account for addition of omniauth and removal of admin member creating functionality.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,29 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
   def create
-    @member = Member.find_by(email: params[:email])
-    if @member && @member.authenticate(params[:password])
-      session[:member_id] = @member.id
-      session[:menu_partial] = "layouts/member_menu"
-      redirect_to member_path(@member), notice: "Welcome back!"
-    else
-      render :new
+    respond_to do |format|
+      if auth_hash = request.env['omniauth.auth']
+        member = Member.find_or_create_by_oauth(auth_hash['info'])
+        session[:member_id] = member.id
+        format.html {
+          redirect_to member_path(member)
+        }
+      else
+        member = Member.find_by(email: params[:email])
+        if member && member.authenticate(params[:password])
+            session[:member_id] = member.id
+            format.html {
+              redirect_to member_path(member)
+            }
+        else
+          format.js {
+            render 'errors',
+              locals:{member:member}
+          }
+        end
+      end
     end
   end
 
@@ -16,4 +33,9 @@ class SessionsController < ApplicationController
     end
     redirect_to root_path
   end
+
+  private
+    def auth
+      request.env['omniauth.auth']
+    end
 end
