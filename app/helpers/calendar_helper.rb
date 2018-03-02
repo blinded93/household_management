a13c344028db6e50f8dde4af6a7ef5973f4d751d
blinded93 <blinded93@gmail.com>
2018-03-01 18:41:04 -0800

    remove display_event and replace with display_bills/chores methods to simplify calendar.

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -3,23 +3,15 @@ module CalendarHelper
     Calendar.new(self, date, block).table
   end
 
-  def bill_events(date)
-    @events[date].select{|e|e.is_a?(Bill)}
+  def display_chore(chores, date)
+    if chores[date]
+      "Chores(#{chores[date].count})"
+    end
   end
 
-  def chore_events(date)
-    @events[date].select{|e|e.is_a?(Chore)}
-  end
-
-  def display_event(event)
-    if event.respond_to?(:utility)
-      content_tag(:div, class:'small bg-danger text-light pl-1 pr-1') do
-        event.utility
-      end
-    elsif event.respond_to?(:task)
-      content_tag(:div, class:'small bg-warning pl-1 pr-1') do
-        event.task
-      end
+  def display_bills(bills, date)
+    if bills[date]
+      "Bills(#{bills[date].count})"
     end
   end
 end
