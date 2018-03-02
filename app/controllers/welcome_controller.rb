621d19ac01d5e9f084549e66e4346fc4aafac36c
blinded93 <blinded93@gmail.com>
2018-03-01 18:37:30 -0800

    add complete action to join/create a household and complete registration. add redirects to home action in case member is created without joining/creating a household.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,24 @@
 class WelcomeController < ApplicationController
   def home
     if logged_in?
-      redirect_to member_path(current_member)
+      if !current_household
+        redirect_to join_complete_path
+      else
+        redirect_to member_path(current_member)
+      end
     end
     @household = Household.new
     @member = @household.members.build
+  end
+
+  def complete
+    if current_member
+      if current_household
+        redirect_to member_path(current_member)
+      end
+    else
+      redirect_to root_path
+    end
+    @household = Household.new
   end
 end
