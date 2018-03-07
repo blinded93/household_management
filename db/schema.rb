fb29776ba4074e6d8f6d73a5926fe3cc7130781e
blinded93 <blinded93@gmail.com>
2018-03-07 09:40:16 -0800

    remove family_title attribute from members

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180225011453) do
+ActiveRecord::Schema.define(version: 20180303215242) do
 
   create_table "bills", force: :cascade do |t|
     t.string   "company"
@@ -82,7 +82,6 @@ ActiveRecord::Schema.define(version: 20180225011453) do
   create_table "members", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "family_title"
     t.integer  "monthly_income",    default: 0
     t.string   "email"
     t.string   "password_digest"
