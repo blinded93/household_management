5e41f17cc917458c58d09f77a3024db66c55c50e
blinded93 <blinded93@gmail.com>
2018-03-08 16:41:22 -0800

    remove unused methods. rename methods to better describe their purpose.

--- lib/joins.rb
+++ lib/joins.rb
@@ -1,10 +1,4 @@
 module Joins
-  def attrs
-    attributes.keys.select do |attr|
-      !attr.include?("_") && !attr.include?("id")
-    end
-  end
-
   def addee
     self.class_name.split("_").first
   end
--- lib/shared.rb
+++ lib/shared.rb
@@ -1,6 +1,6 @@
 module Shared
   def class_name
-    a ||= self.class.name.split(/(?=[A-Z])/)
+    a = self.class.name.split(/(?=[A-Z])/)
     a.join("_").downcase
   end
 
@@ -8,49 +8,23 @@ module Shared
     class_name.pluralize
   end
 
-  def id_name(field)
+  def form_input_id(field)
     a = [class_name, "_"]
-    if field == :item_id
-      a << "item_attributes_name"
-    else
-      a << field.to_s
-    end
-    if persisted?
-      a.insert(2, self.id.to_s + "_")
-    end
+    a.push(field == :item_id ? "item_attributes_name" : field.to_s)
     a.join.html_safe
   end
 
-  def error_div
+  def error_div_id
     a = [class_name, "_", "errors"]
-    if persisted?
-      a.insert(2, self.id.to_s + "_")
-    end
     a.join.html_safe
-  end
-
-  def tbody_id(scope)
-    [plural_name, "_", scope.to_s, "_tbody"].join.html_safe
   end
 
   def row_id
     [class_name, "_", id.to_s].join.html_safe
   end
 
-  def row_partial
-    [class_name, "s/", class_name].join.html_safe
-  end
-
   def completed_fields
     cols - errors.keys
-  end
-
-  def reload_div(scope)
-    [scope.to_s, "_", plural_name, "_collapse"].join.html_safe
-  end
-
-  def scope_table(scope)
-    [plural_name, "_", scope.to_s, "_tbody"].join.html_safe
   end
 
   def badge_span(scope)
