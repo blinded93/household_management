24099461a59938a79fd1e38a46e70f789dd5bd91
blinded93 <blinded93@gmail.com>
2018-03-13 18:27:52 -0700

    move #menu_link method to applicationhelper to be used by member#show page as well.

--- app/helpers/households_helper.rb
+++ app/helpers/households_helper.rb
@@ -1,15 +1,2 @@
 module HouseholdsHelper
-  def menu_link(link_name)
-    link_to link_name,
-      "##{link_name.downcase}",
-      class:"btn btn-sm btn-outline-secondary text-dark",
-      data:{
-        parent:"#admin-control",
-        toggle:"collapse"
-      },
-      aria:{
-        expanded:"false",
-        controls:"#{link_name.downcase}"
-      }
-  end
 end
