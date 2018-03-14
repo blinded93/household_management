064bd17274cf353612d11aa57191fe8b2b5bdfa8
blinded93 <blinded93@gmail.com>
2018-03-13 18:30:48 -0700

    move card methods from householdhelpers to be used by member#show as well. organized related methods.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -46,6 +46,16 @@ module ApplicationHelper
       str + " badge-success"
     end
   end
+  # Dialog form
+  def form_header(obj)
+    content_for :header do
+      if item?(obj)
+        obj.new_record? ? "Add Item" : "Edit #{obj.item.name}"
+      else
+        obj.new_record? ? "Create #{obj.class}" : "Edit #{obj.class}"
+      end
+    end
+  end
 
   def dialog_form_path(obj)
     case obj
@@ -58,32 +68,59 @@ module ApplicationHelper
     end
   end
 
-  def dialog_submit_btn(f)
-    obj = f.object
-    f.submit((obj.new_record? ? "Create" : "Update"),
-      class:btn,
-      form:( obj.new_record? ? "new_#{obj.class_name}" : "edit_#{obj.row_id}" )
-    ).html_safe
-  end
-
   def form_render(f)
     render "#{f.object.plural_name}/form", f:f
-  end
-
-  def form_header(obj)
-    content_for :header do
-      if item?(obj)
-        obj.new_record? ? "Add Item" : "Edit #{obj.item.name}"
-      else
-        obj.new_record? ? "Create #{obj.class}" : "Edit #{obj.class}"
-      end
-    end
   end
 
   def form_error_div(obj)
     content_tag :div,
       id:obj.error_div_id,
       class:errors do
+    end
+  end
+
+  def dialog_submit_btn(f)
+    obj = f.object
+    f.submit((obj.new_record? ? "Create" : "Update"),
+    class:btn,
+      form:(obj.new_record? ? "new_#{obj.class_name}" : "edit_#{obj.row_id}")
+    ).html_safe
+  end
+  # Main menu
+  def menu_link(parent, link_name)
+    link_to link_name,
+      "##{link_name.downcase}",
+      class:"btn btn-sm btn-outline-secondary text-dark",
+      data:{
+        parent:"##{parent}",
+        toggle:"collapse"
+      },
+      aria:{
+        expanded:"false",
+        controls:"#{link_name.downcase}"
+      }
+  end
+  # Collapse windows
+  def link_div(key, value)
+    content_tag :div,
+      class:'link' do
+        collapse_div(key, value)
+    end
+  end
+
+  def collapse_div(key, value)
+    content_tag :div,
+      class:"collapse #{" show" if key == :calendar}",
+      id:key.to_s do
+        card_div(key, value)
+    end
+  end
+
+  def card_div(key, value)
+    content_tag :div,
+      class:'card card-block p-3' do
+        dir = (key == :calendar ? key.to_s : value.pluralize)
+        admin? ? render "#{dir}/admin_#{value}_card" : render "#{dir}/member_#{value}_card"
     end
   end
 end
